82f416e94aa90f5b6c0a958c21d88cd11207e282
Mark Smith <smith.h.mark@gmail.com>
2014-08-23 13:15:44 -0700

    correcting import error

--- snippets/models.py
+++ snippets/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from pygments.lexer import get_all_lexers
+from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
