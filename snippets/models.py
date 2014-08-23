e7204a5a5e059166baabe9e5f732ba3ac1004011
Mark Smith <smith.h.mark@gmail.com>
2014-08-23 13:50:16 -0700

    fixing typos

--- snippets/models.py
+++ snippets/models.py
@@ -11,10 +11,10 @@ class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
-    lineos = models.BooleanField(default=False)
+    linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', 
         max_length=100)
-    sytle = models.CharField(choices=STYLE_CHOICES, default='friendly',
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly',
         max_length=100)
     class Meta:
         ordering = ('created',)
