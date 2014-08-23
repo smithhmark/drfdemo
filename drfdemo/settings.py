ce67accd230f4f94d3f6d214f274e6a50c299733
Mark Smith <smith.h.mark@gmail.com>
2014-08-23 11:14:26 -0700

    updating database config

--- drfdemo/settings.py
+++ drfdemo/settings.py
@@ -58,7 +58,11 @@ WSGI_APPLICATION = 'drfdemo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'tmp.db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
