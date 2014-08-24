d4772755b1b384803dfb20256cb2ef5002d70054
Mark Smith <smith.h.mark@gmail.com>
2014-08-23 20:05:08 -0700

    migrating snippet list from function to object

--- snippets/views.py
+++ snippets/views.py
@@ -1,20 +1,21 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.http import Http404
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
-@api_view(['GET', 'POST'])
-def snippet_list(request, format=None):
+class SnippetList(APIView):
     """
     List all code snippets, or create a new snippet.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = SnippetSerializer(data=request.DATA)
         if serializer.is_valid():
             serializer.save()
